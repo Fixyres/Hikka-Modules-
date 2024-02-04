79cbcfe3bce560bb32d827c6dc78d67731bb0f81
Fixyres <158840682+Fixyres@users.noreply.github.com>
2024-02-04 21:14:03 +0200

    Update calculatorhikka.py

--- calculatorhikka.py
+++ calculatorhikka.py
@@ -8,14 +8,33 @@ class CalculatorModule(loader.Module):
 
     strings = {"name": "Calculator"}
 
+    def __init__(self):
+        self.round_down = False
+
+    @loader.unrestricted
+    @loader.command("rounddown")
+    async def zi(self, message):
+        """Включить/выключить закругление до меньшего числа."""
+        self.round_down = not self.round_down
+        await utils.answer(message, f"{'🔄 Включено' if self.round_down else '🔄 Выключено'} округление до меньшего числа.")
+
     @loader.command("calc", "<expression>", aliases=["calculate"])
     async def calc(self, message):
-        """⠀"""
+        """Вычислить математическое выражение."""
         expression = utils.get_args_raw(message)
 
         try:
             result = sympify(expression)
-            result_str = f"{result.evalf():.5e}" if abs(result.evalf()) > 1e6 else str(result.evalf())
+            if isinstance(result, Rational):
+                if self.round_down:
+                    result_str = f"{int(result.evalf())}"
+                else:
+                    result_str = f"{result.evalf()}"
+            else:
+                if self.round_down:
+                    result_str = f"{int(result)}"
+                else:
+                    result_str = f"{result}"
             await utils.answer(message, f"🧮 <b>{expression} = {result_str}</b>")
         except Exception as e:
             error_message = await utils.answer(message, f"⚠️ <b>Ой! Ошибка!</b>")
